148a101217b0c4f08975aea1f4e54b1591562637
天野智広 <amanotomohiro@amanotomohiro-no-macbook-pro.local>
2019-02-10 07:40:47 +0900

    課題に取り組む前に一旦保存 AppDelegateを誤って上書きしてしまった。 その中身についてはテキストの内容をコピペ

---
+++ taskapp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 天野智広 on 2019/02/06.
+//  Copyright © 2019 天野智広. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
